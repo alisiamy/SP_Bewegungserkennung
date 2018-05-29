da529ca438a79a8b20252bd30fa224cc956ecda8
Darija Grisanova <darija.grisanova@st.ovgu.de>
2018-05-30 01:30:50 +0200

    mah distance start

--- clustering.cs
+++ clustering.cs
@@ -4,9 +4,17 @@ using System.Collections.Generic;
 namespace tryCluster{
     public struct point{
         public double x, y;
-        public point(int px, int py){
+        public point(double px, double py){
             x = px;
             y = py;
+        }
+        public void add(point ipt){
+            this.x += ipt.x;
+            this.y += ipt.y;
+        }
+        public void divide(int c){
+            this.x /= c;
+            this.y /= c;
         }
     }
     class Cluster{
@@ -14,7 +22,16 @@ namespace tryCluster{
         HashSet <point> CL;
         point mean;
         public Cluster(point ipt){
+            mean = ipt;
             addToCluster(CL, ipt);
+        }
+
+        public void calclulateMean(){
+            point tmpSum = new point(0,0);
+            foreach(point n in CL){
+                tmpSum.add(n);
+            }
+            tmpSum.divide(CL.Count);
         }
         public void addToCluster(HashSet <point> cl, point ipt){
             cl.Add(ipt);
@@ -23,10 +40,11 @@ namespace tryCluster{
     }
 
     class KMclustering{
-        public int k = 1;
+        public int k = 1; // Konstruktor fuer k?
         public LinkedList<Cluster> CLlist;
         
         public void clustering(point[] ipt){
+            // Initial centroids for clusters according to k
             HashSet <int> iSet = new HashSet<int>();
             while(iSet.Count < k){
             Random ran = new Random();
@@ -38,6 +56,10 @@ namespace tryCluster{
             foreach(int i in iSet){
                 CLlist.AddLast(new Cluster(ipt[i]));
             }
+
+            do {
+
+            }while();
         }
 
     }
